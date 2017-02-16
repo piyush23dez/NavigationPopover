801e8a759b2d3dd6c525ce5a3c2d3ada7bd55563
Piyush Sharma <piyush_sharma2@apple.com>
2017-02-15 16:16:42 -0800

    first commit

--- PopoverNavigation/ViewController.swift
+++ PopoverNavigation/ViewController.swift
@@ -20,6 +20,44 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+}
 
+class PopoverTableViewController: UITableViewController {
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell()
+        cell.textLabel?.text = String(indexPath.row)
+        return cell
+    }
+    
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 40
+    }
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+       
+       let vc = UIStoryboard(name: "Main", bundle: nil).instantiateViewController(withIdentifier: "pushedViewController") as! SampleViewControler
+        
+        let backItem = UIBarButtonItem()
+        backItem.title = "Back"
+        navigationItem.backBarButtonItem = backItem
+        self.navigationController?.pushViewController(vc, animated: true)
+    }
+}
+
+
+class SampleViewControler: UIViewController {
+    
+    override func viewDidLoad() {
+        navigationItem.title = "DetailedTableViewCell"
+    }
 }
 
